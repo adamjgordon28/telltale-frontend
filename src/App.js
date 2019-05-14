47b1589a1efc55d8e0600e38e62646e034a2b6d2
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-14 14:45:37 -0400

    up and running

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Editor, EditorState, RichUtils} from "draft-js";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    }
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  makeBold(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'BOLD'
+    ))
+  }
+
+  makeItalic(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'ITALIC'
+    ))
+  }
+
+  makeUnderlined(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'UNDERLINE'
+    ))
+  }
+
+  makeHighlighted(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'HIGHLIGHT'
+    ))
+  }
+
+
+  onChange =(editorState) => {
+    this.setState({
+      editorState: editorState
+    })
+  }
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+
+ createNote = (noteContent) => {
+  fetch("http://localhost:4000/api/v1/entries", {
+   method: "post",
+   headers: { "Content-Type": "application/json", "Accepts": "application/json" },
+   body: JSON.stringify({ content: JSON.stringify(noteContent) })
+  })
+   .then(response => response.json())
+   .then(json => {
+    console.log(json)
+   })
+}
+
+handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+
+
+
+  render() {
+    return (
+    <div>
+    <button onClick={() => {this.makeBold();}}>Bold</button>
+    <Editor
+    onChange={(editorState) => {this.onChange(editorState)}}
+    editorState={this.state.editorState}
+    handleKeyCommand={this.handleKeyCommand}
+    />
     </div>
-  );
+  )};
+
 }
 
 export default App;
