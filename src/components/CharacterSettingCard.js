dfe34e83ec53cfc808ac857b25a2396b972bccc2
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-26 15:45:27 -0400

    styled cards on storyboard

--- src/components/CharacterSettingCard.js
+++ src/components/CharacterSettingCard.js
@@ -10,10 +10,10 @@ class CharacterSettingCard extends Component {
     return(
 
             <div className="ui card" style={{position: "relative",left:"6%", display: "inline-block", width: "40%", height: "16em", margin: "2%"}}>
-          <div style={{position: "absolute", wordWrap:"break-word", fontsize: "1.25"}} className="header"><h4>{this.props.charSet.character.name} at {this.props.charSet.setting.name}</h4></div>
-          <div style={{position: "absolute", top: "25%", color:"gray", fontWeight: "bold"}}><span className="category">Chapter {this.props.charSet.chapter}</span></div>
-          <div style={{position: "absolute", top: "36%", right: ".25%", maxHeight: "37.5%", minHeight: "37.5%", overflowY: "scroll", width: "100%", border:".125em beige solid"}} className="description">
-          <h3>Description:</h3>
+          <div className="header"><h4 style={{fontSize:"1em", wordWrap:"break-word", overflowX: "hidden", overflowY: "hidden", minHeight: "3.75em", maxHeight:"3.75em"}}>{this.props.charSet.character.name} at {this.props.charSet.setting.name}</h4></div>
+          <div style={{position: "absolute", top: "27.5%", color:"gray", fontWeight: "bold"}}><span className="category">Chapter {this.props.charSet.chapter}</span></div>
+          <div style={{position: "absolute", top: "40%", right: ".25%", maxHeight: "37.5%", minHeight: "37.5%", overflowY: "scroll", width: "100%", border:".125em beige solid"}} className="description">
+          <h5>Description:</h5>
             <p>{this.props.charSet.description}</p>
           </div>
         <div className="extra content">
