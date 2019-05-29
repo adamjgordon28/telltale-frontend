e15c5fa0674b8c49dced17aed47d219d40c65419
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-28 20:55:30 -0400

    got profile working somewhat

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -10,7 +10,8 @@ class Profile extends React.Component {
     user: null
   }
 
-  componentDidMount = () => {
+
+  fetchAndSetUser = () => {
     fetch("http://localhost:4000/api/v1/users/".concat(`${this.props.match.params.id}`))
     .then(res=>res.json())
     .then(returnedUser => {
@@ -20,20 +21,35 @@ class Profile extends React.Component {
     })
   }
 
+  componentDidMount = () => {
+    this.fetchAndSetUser()
+  }
+
+  componentDidUpdate = (prevProps) => {
+  if (this.props.match.params.id !== prevProps.match.params.id) {
+    this.fetchAndSetUser()
+  }
+
+}
+
   render(){
-    console.log(this.state.user)
     if(this.props.currentUser === -1){
       history.push("/login")
     }
-
+    if(!this.state.user || !this.props.currentUser){
+      return <h1>Loading...</h1>
+    }
     return (
       <div>
-      {this.state.user ? <div><h1>This is the profile of {this.state.user.name}!</h1>
+       {this.state.user.id === this.props.currentUser.id ? <h1>Welcome back {this.state.user.name}</h1> : <h1>This is the profile of {this.state.user.name}</h1> }
+      {this.state.user ? <div>
       <img style={{borderRadius: "50%", height: "12em"}} src={this.state.user.img_url} alt=""/> </div>: null}
+      <button className="ui button positive">Follow this User</button>
       </div>
     )
   }
 }
+
 
 const mapStateToProps = (state) => {
   return {
