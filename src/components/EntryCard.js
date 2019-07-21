695be9768f1b38a1f4b855b04e55f1acbbae9f3f
adamjgordon28 <adamjgordon24@gmail.com>
2019-07-21 19:35:21 -0400

    fixed issue with title overflow

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -29,13 +29,13 @@ class EntryCard extends React.Component {
     return (
       <div style={{width: "40%", minWidth: "40em", height: "18em", position: "relative", top:"5em", marginLeft: "5%", marginTop: "2.5em", display: "inline-block"}} className="ui card">
   <div className="content">
-    <div className="header">{this.props.entry.title.concat("    ")} <span style={{fontSize:".75em", color: "gray"}}> ({this.props.entry.genre})</span>
+    <div className="header"><span style={{display:"inline-block", maxWidth:"20em", overflowX:"hidden", height:"1.25em", whiteSpace:"no-wrap", textOverflow:"ellipsis", position:"relative", top:".25em"}}>{this.props.entry.title.concat("    ")}</span> <span style={{display:"inline-block", position:"relative", marginLeft:"1em",fontSize:".75em", color: "gray"}}> ({this.props.entry.genre})</span>
     <img className="ui avatar image" style={{height:"2em", width: "2em", position:"absolute", right: "2%", top: "2.5%"}} src={logoMatcher[this.props.entry.genre]} alt =""/></div>
   </div>
 
   <div className="extra content">
 
-  <div style={{position:"absolute", right: ".00001%", bottom: "25.5%", maxHeight: "55%", minHeight: "55%", overflowY: "scroll", width: "100%", padding:"1%"}} className="description">
+  <div style={{position:"absolute", right: ".00001%", bottom: "22.5%", maxHeight: "55%", minHeight: "55%", overflowY: "scroll", width: "100%", padding:"1%"}} className="description">
     <p style={{margin:"1.75%"}}> Description: {this.props.entry.description}</p>
   </div>
 
--- src/components/ReadOnlyEntryCard.js
+++ src/components/ReadOnlyEntryCard.js
@@ -29,14 +29,14 @@ class EntryCard extends React.Component {
     return (
       <div style={{width: "40%", minWidth: "40em", height: "18em", position: "relative", top:"5em", marginLeft: "5%", marginTop: "2.5em", display: "inline-block"}} className="ui card">
   <div className="content">
-    <div className="header">{this.props.entry.title.concat("    ")} <span style={{fontSize:".75em", color: "gray"}}> ({this.props.entry.genre})</span>
+    <div className="header"><span style={{display:"inline-block", maxWidth:"20em", overflowX:"hidden", height:"1.25em", whiteSpace:"no-wrap", textOverflow:"ellipsis", position:"relative", top:".25em"}}>{this.props.entry.title.concat("    ")}</span> <span style={{display:"inline-block", position:"relative", marginLeft:"1em", fontSize:".75em", color: "gray"}}> ({this.props.entry.genre})</span>
     <img className="ui avatar image" style={{height:"2em", width: "2em", position:"absolute", right: "2%", top: "2.5%"}} src={logoMatcher[this.props.entry.genre]} alt =""/></div>
   </div>
 
   <div className="extra content">
 
-  <div style={{position:"absolute", right: ".00001%", bottom: "25.5%", maxHeight: "55%", minHeight: "55%", overflowY: "scroll", width: "100%", padding:"1%"}} className="description">
-    <h4 style={{position:"relative", left:"1.625%", color:"black"}}>by {this.props.entry.user.username}</h4>
+  <div style={{position:"absolute", right: ".00001%", bottom: "22.5%", maxHeight: "55%", minHeight: "55%", overflowY: "scroll", width: "100%", padding:"1%"}} className="description">
+    <h4 style={{position:"relative", left:"1.625%", color:"black", top:".25em"}}>by {this.props.entry.user.username}</h4>
     <p style={{margin:"1.625%"}}> Description: {this.props.entry.description}</p>
   </div>
 
